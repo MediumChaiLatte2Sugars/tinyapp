8d9a837b3953bd2e6eb18c8c0f0dce2210bea4f1
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-01-13 19:41:29 -0500

    Test: userLookup must return user obj with email

---
+++ test/helpersTest.js
@@ -0,0 +1,25 @@
+const { assert } = require('chai');
+
+const { userLookup } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('userLookup', function() {
+  it('should return a user with valid email', function() {
+    const should = require('chai').should()
+      , user = userLookup("user@example.com", testUsers)
+      , expectedUserID = "userRandomID";
+    user.should.be.a('object').and.have.a.property('id').with.a.valueOf(expectedUserID);
+  });
+});
