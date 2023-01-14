8cf638b97e1657f49fb6ace04c9e0621b33e7b1d
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-01-14 00:57:43 -0500

    Refactor: Modularize and extract all helper functions Move the remaining helper functions into the helpers.js file and modularize them for testing purposes.

--- helpers.js
+++ helpers.js
@@ -12,4 +12,64 @@ function userLookup(email, database){
   }
 }
 
-module.exports = { userLookup };
+/**
+ * Helper function for checking if a user (by id) has access to a given URL
+ * @param {*} id 
+ * @param {*} reqURL 
+ * @returns true if the user is authorized, false otherwise
+ */
+function checkURLAuth(reqURL, userURLS){
+
+  // Check if req URL contained in collection
+  for (let urlObj of userURLS){
+   
+    if (urlObj.longURL === reqURL){
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * Helper function for obtaining all URLs associated with a
+ * given user id
+ * @param {*} id 
+ * @returns an array of URL objects
+ */
+function urlsForUser(id, urlDatabase){
+  let userURL = [];
+  
+  for (let urlObj in urlDatabase){
+    
+    let urlToAdd;
+    
+    if (urlDatabase[urlObj].userID === id){
+      urlToAdd = {
+        id: urlObj,
+        longURL: urlDatabase[urlObj].longURL,
+      }
+      userURL.push(urlToAdd);
+    }
+  }
+  return userURL;
+}
+
+/**
+ * Generates and returns a string of 6 random alphanumeric characters
+ * @returns a string of 6 random alphanumeric characters
+ */
+function generateRandomString() {
+  const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const STRING_LENGTH = 6;
+
+  let result = '';
+  const charactersLength = characters.length;
+  
+  for ( let i = 0; i < STRING_LENGTH; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+module.exports = { userLookup, checkURLAuth, urlsForUser, generateRandomString };
